5d928beee6e4d9d854b708a6be30559738b49d2e
Sharad Bhat <sharad.mbhat@gmail.com>
2017-08-09 00:26:21 +0530

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,19 @@
+from setptools import setup
+
+setup(
+    name='Zomatopy',
+    version='1.0.0',
+    url='https://github.com/sharadbhat/Zomatopy',
+    description='A simple python wrapper for the Zomato API',
+    author='Sharad Bhat',
+    author_profile='https://github.com/sharadbhat',
+    license='MIT',
+    classifiers=[
+        'Intended Audience :: Developers',
+        'Programming Language :: Python :: 3.6',
+    ],
+    keywords='Zomato',
+    install_requires=[
+        'requests==2.11.1'
+    ]
+)
