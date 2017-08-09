f886f79d190dc53060f9cac1c5f152dc9157990f
Sharad Bhat <sharad.mbhat@gmail.com>
2017-08-10 00:24:22 +0530

    Update zomatopy.py

--- zomatopy/zomatopy.py
+++ zomatopy/zomatopy.py
@@ -47,6 +47,7 @@ class Zomato:
         self.is_rate_exceeded(a)
 
         if len(a['location_suggestions']) == 0:
+            raise ValueError('InvalidCityName')
         elif 'name' in a['location_suggestions'][0]:
             city_name = city_name.replace('%20', ' ')
             if str(a['location_suggestions'][0]['name']).lower() == str(city_name).lower():
