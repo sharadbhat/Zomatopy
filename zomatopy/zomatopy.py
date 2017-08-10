94b7bff75a321592aa6af25f1134c414f682b3d6
Sharad Bhat <sharad.mbhat@gmail.com>
2017-08-10 23:58:01 +0530

    Update zomatopy.py

--- zomatopy/zomatopy.py
+++ zomatopy/zomatopy.py
@@ -15,6 +15,7 @@ class Zomato:
 
     def get_categories(self):
         """
+        Takes no input.
         Returns a dictionary of IDs and their respective category names.
         """
         headers = {'Accept': 'application/json', 'user-key': self.user_key}
@@ -33,6 +34,7 @@ class Zomato:
 
     def get_city_ID(self, city_name):
         """
+        Takes City Name as input.
         Returns the ID for the city given as input.
         """
         if city_name.isalpha() == False:
@@ -47,20 +49,22 @@ class Zomato:
         self.is_rate_exceeded(a)
 
         if len(a['location_suggestions']) == 0:
-            raise ValueError('InvalidCityName')
+            raise Exception('invalid_city_name')
         elif 'name' in a['location_suggestions'][0]:
             city_name = city_name.replace('%20', ' ')
             if str(a['location_suggestions'][0]['name']).lower() == str(city_name).lower():
                 return a['location_suggestions'][0]['id']
+            else:
+                raise ValueError('InvalidCityId')
 
 
     def get_city_name(self, city_ID):
         """
+        Takes City ID as input.
         Returns the name of the city ID given as input.
         """
-        city_ID = str(city_ID)
-        if city_ID.isnumeric() == False:
-            raise ValueError('InvalidCityId')
+        self.is_valid_city_id(city_ID)
+
         headers = {'Accept': 'application/json', 'user-key': self.user_key}
         r = (requests.get(base_url + "cities?city_ids=" + str(city_ID), headers=headers).content).decode("utf-8")
         a = ast.literal_eval(r)
@@ -74,19 +78,16 @@ class Zomato:
             temp_city_ID = a['location_suggestions'][0]['id']
             if temp_city_ID == str(city_ID):
                 return a['location_suggestions'][0]['name']
-            else:
-                raise ValueError('InvalidCityId')
 
 
 
     def get_collections(self, city_ID, limit=None):
         """
+        Takes City ID as input. limit parameter is optional.
         Returns dictionary of Zomato restaurant collections in a city and their respective URLs.
-        limit parameter is optional and must be an integer.
         """
-        city_ID = str(city_ID)
-        if city_ID.isnumeric() == False:
-            raise ValueError('InvalidCityId')
+        self.is_valid_city_id(city_ID)
+
         headers = {'Accept': 'application/json', 'user-key': self.user_key}
         if limit == None:
             r = (requests.get(base_url + "collections?city_id=" + str(city_ID), headers=headers).content).decode("utf-8")
@@ -110,11 +111,11 @@ class Zomato:
 
     def get_cuisines(self, city_ID):
         """
+        Takes City ID as input.
         Returns a sorted dictionary of all cuisine IDs and their respective cuisine names.
         """
-        city_ID = str(city_ID)
-        if city_ID.isnumeric() == False:
-            raise ValueError('InvalidCityId')
+        self.is_valid_city_id(city_ID)
+
         headers = {'Accept': 'application/json', 'user-key': self.user_key}
         r = (requests.get(base_url + "cuisines?city_id=" + str(city_ID), headers=headers).content).decode("utf-8")
         a = ast.literal_eval(r)
@@ -138,11 +139,11 @@ class Zomato:
 
     def get_establishment_types(self, city_ID):
         """
+        Takes City ID as input.
         Returns a sorted dictionary of all establishment type IDs and their respective establishment type names.
         """
-        city_ID = str(city_ID)
-        if city_ID.isnumeric() == False:
-            raise ValueError('InvalidCityId')
+        self.is_valid_city_id(city_ID)
+
         headers = {'Accept': 'application/json', 'user-key': self.user_key}
         r = (requests.get(base_url + "establishments?city_id=" + str(city_ID), headers=headers).content).decode("utf-8")
         a = ast.literal_eval(r)
@@ -161,21 +162,111 @@ class Zomato:
 
             return establishment_types
         else:
-            raise ValueError('invalid_city_id')
+            raise ValueError('InvalidCityId')
 
 
 
-    def get_daily_menu(self, restaurant_ID):
+    def get_nearby_restaurants(self, latitude, longitude):
         """
+        Takes the latitude and longitude as inputs.
+        Returns a dictionary of Restaurant IDs and their corresponding Zomato URLs.
         """
-        pass
+        try:
+            float(latitude)
+            float(longitude)
+        except ValueError:
+            raise ValueError('InvalidLatitudeOrLongitude')
+
+        headers = {'Accept': 'application/json', 'user-key': self.user_key}
+        r = (requests.get(base_url + "geocode?lat=" + str(latitude) + "&lon=" + str(longitude), headers=headers).content).decode("utf-8")
+        a = ast.literal_eval(r)
+
+        nearby_restaurants = {}
+        for nearby_restaurant in a['nearby_restaurants']:
+            nearby_restaurants.update({nearby_restaurant['restaurant']['id'] : nearby_restaurant['restaurant']['url']})
+
+        return nearby_restaurants
+
+
+
+    def get_restaurant(self, restaurant_ID):
+        """
+        Takes Restaurant ID as input.
+        Returns a dictionary of restaurant details.
+        """
+        self.is_valid_restaurant_id(restaurant_ID)
+
+        headers = {'Accept': 'application/json', 'user-key': self.user_key}
+        r = (requests.get(base_url + "restaurant?res_id=" + str(restaurant_ID), headers=headers).content).decode("utf-8")
+        a = ast.literal_eval(r)
+
+        if 'code' in a:
+            if a['code'] == 404:
+                raise('InvalidRestaurantId')
+
+        restaurant_details = {}
+        restaurant_details.update({"name" : a['name']})
+        restaurant_details.update({"url" : a['url']})
+        restaurant_details.update({"location" : a['location']['address']})
+        restaurant_details.update({"city" : a['location']['city']})
+        restaurant_details.update({"city_ID" : a['location']['city_id']})
+        restaurant_details.update({"user_rating" : a['user_rating']['aggregate_rating']})
+
+        restaurant_details = DotDict(restaurant_details)
+        return restaurant_details
+
+
+    def restaurant_search(self, query="", latitude="", longitude="", cuisines="", limit=5):
+        """
+        Takes either query, latitude and longitude or cuisine as input.
+        Returns a list of Restaurant IDs.
+        """
+        cuisines = "%2C".join(cuisines.split(","))
+        if str(limit).isalpha() == True:
+            raise ValueError('LimitNotInteger')
+        headers = {'Accept': 'application/json', 'user-key': self.user_key}
+        r = (requests.get(base_url + "search?q=" + str(query) + "&count=" + str(limit) + "&lat=" + str(latitude) + "&lon=" + str(longitude) + "&cuisines=" + str(cuisines), headers=headers).content).decode("utf-8")
+        a = ast.literal_eval(r)
+
+        restaurants = []
+
+        if a['results_found'] == 0:
+            return []
+        else:
+            for restaurant in a['restaurants']:
+                restaurants.append(restaurant['restaurant']['id'])
+
+        return restaurants
+
+
+
+
+    def is_valid_restaurant_id(self, restaurant_ID):
+        """
+        Checks if the Restaurant ID is valid or invalid.
+        If invalid, throws a InvalidRestaurantId Exception.
+        """
+        restaurant_ID = str(restaurant_ID)
+        if restaurant_ID.isnumeric() == False:
+            raise ValueError('InvalidRestaurantId')
+
+
+
+    def is_valid_city_id(self, city_ID):
+        """
+        Checks if the City ID is valid or invalid.
+        If invalid, throws a InvalidCityId Exception.
+        """
+        city_ID = str(city_ID)
+        if city_ID.isnumeric() == False:
+            raise ValueError('InvalidCityId')
 
 
 
     def is_key_invalid(self, a):
         """
         Checks if the API key provided is valid or invalid.
-        If invalid, throws a invalid_key Exception.
+        If invalid, throws a InvalidKey Exception.
         """
         if 'code' in a:
             if a['code'] == 403:
@@ -186,8 +277,19 @@ class Zomato:
     def is_rate_exceeded(self, a):
         """
         Checks if the request limit for the API key is exceeded or not.
-        If exceeded, throws a API_limit_exceeded Exception.
+        If exceeded, throws a ApiLimitExceeded Exception.
         """
         if 'code' in a:
             if a['code'] == 440:
                 raise Exception('ApiLimitExceeded')
+
+
+
+class DotDict(dict):
+    """
+    Dot notation access to dictionary attributes
+    """
+
+    __getattr__ = dict.get
+    __setattr__ = dict.__setitem__
+    __delattr__ = dict.__delitem__
